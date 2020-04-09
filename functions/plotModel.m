e287ae3009fb4e759d18c57ac54d9f443b565c80
karinbo <63347082+karinbo@users.noreply.github.com>
2020-04-09 12:06:11 +0200

    Update plotModel.m

--- functions/plotModel.m
+++ functions/plotModel.m
@@ -1,28 +1,41 @@
-function [fh,plottedModel] = plotModel(model,plotOpts,figurehandle)
-% plotModel - function to plot the fitted mixed effect model
+function outModelVect = fitOptModel(input, opts)
+% fitOptModel - function to fit a random intersect models of order 0 to 3 
+% to the input data and return the best model according to the BIC
 %
-% Syntax:  fh = plotModel(model,[figurehandle])
+% Syntax:  model = fitOptModel(input, opts)
 %
 % Inputs:
-%    model - structure containing estimated mixed-effect model, see output
-%       of estimateModel() for more details
-%    plotOpts - struct with plotting color and the axis label and legend 
-%       information, if empty, the variable names will be used as labels
-%       [.plotType] = 'full' - always plot full model, even if interation or 
-%                       intercept are not significantly different
-%                'redInter' - plot reduced model without interaction if 
-%                       interaction is not significant
-%                'redGrp' - plot reduced model without group effect if 
-%                       group effect is not significant 
-%       [.plotCol] - cellstring with plotting colors
-%       [.plotCI] - 0 or 1 (default) indicating if confidence bands should be plotted
-%    figurehandle - handle to figure where to plot the graph, if emty or
-%       not existent a new figure is created
+%    input - structure containing the input information:
+%           .subjID - vector of subject IDs (double, #sub x 1)
+%           .age - vector of ages (double, #sub x 1)
+%           .grouping - vector with grouping information (double/logical)
+%           .data - matrix with data to model (double, #sub x #models)
+%           [.cov] - matrix with covariates to consider
+%    opts - structure with model fitting options
+%           [.vertID] - columns of input.data to analyze
+%           [.modelNames] - names of models, default = index
+%           .orders - vector of model orders to check for
+%           [.alpha] - limit for significance check, default = 0.05
+%           [.mType] - 'intercept' (default), random intersect model
+%                      'slope' for random slope model
 %
 % Outputs:
-%    fh - handle of plotted figure
-%    plottedModel - string indicating which model was plotted:
-%           'full','noGroup' or 'noInteraction'
+%    outModelVect - vector of structs with fitted optimum models (#models x 1)
+%           .mName - name of model according to opts
+%           .input - estimation inputs
+%           .order - order of resulting model
+%           .designMatrix - design matrix of estimated model
+%           .designVars - names of columns of the design matrix
+%           .beta - vector of estimated fixed effects (betas)
+%           .randCov - covariance matrix of estimated random effect
+%           .stats  - structure with model fitting statistics (see nlmefit
+%                   for details)
+%           .groupEffect - structure with significance of group difference 
+%               (.h and .p) and estimated reduced model (.reducedModel)
+%               without grouping variable
+%           .interEffect - structure with significance of age*group 
+%               interaction effect (.h and .p) and estimated reduced model 
+%               (.reducedModel) without interaction
 %
 % Example: 
 %
@@ -33,208 +46,117 @@ function [fh,plottedModel] = plotModel(model,plotOpts,figurehandle)
 % v1.0 3.8.2016 DZ - initial version based on initial code of Kadir Mutlu
 
 %% set up defaults
-if nargin < 3 || isempty(figurehandle)
-    fh=figure;
+if ~isfield(opts,'alpha')
+    opts.alpha=0.05;
+end
+
+if ~isfield(opts,'vertID')
+    opts.vertID=1:size(input.data,2);
+end
+
+if ~isfield(opts,'mType')
+    opts.mType='intercept';
+end
+
+if ~isfield(input,'cov')
+    input.cov=[];
 else
-    fh = figurehandle;
-end
-
-if nargin < 2
-    plotOpts = struct;
-end
-
-if ~isfield(plotOpts,'plotCol')
-    plotCol = {[0 0 1],[1 0 0],[0 0 0]};
-else
-    plotCol = plotOpts.plotCol;
-end
-
-if ~isfield(plotOpts,'plotType')
-    plotOpts.plotType = 'redGrp';
-end
-
-nCov = plotOpts.nCov;%length(model.beta)-2*(model.order+1);
-
-%% check which model to plot 
-% (reduced model if group/interaction effects are not significant)
-grouping = model.input.grouping;
-groups=unique(grouping);
-
-for iG = 1:length(groups)
-    data{iG}=model.input.data(grouping==groups(iG));
-    age{iG}=model.input.age(grouping==groups(iG));
-    subjects{iG}=model.input.subjID(grouping==groups(iG));
+    input.cov=input.cov-repmat(mean(input.cov),size(input.cov,1),1); % center covariates to avoid confounds
 end
 
 
-figure(fh);
-if strcmp(plotOpts.plotType,'redGrp') && (~isfield(model,'groupEffect') || ~model.groupEffect.h) % no significant group effect
-    plModel = model.groupEffect.reducedModel;
+%% initialize
+nModels=length(opts.vertID);
+nOrders=length(opts.orders);
+
+%% estimate models and return best fit according to BIC
+for iM = 1:nModels
+    fprintf('\nModel %d : ',opts.vertID(iM));
+    iV=opts.vertID(iM);
+    dataVect=input.data(:,iV);
     
-    % get model parameters
-    params = plModel.beta(1:end-nCov)';
+    if any(isnan(dataVect))
+        warning('Your data vector is not complete! Running analysis without missing data points');
+    end
+    dataID=~isnan(dataVect);
     
-    % plot data
-    for iG = 1:length(groups)
-        pl(iG) = plotDataAndFit(age{iG},data{iG},subjects{iG},[],plotCol{iG});
+    if ~any(dataVect) %
+        if isfield(opts,'modelNames')
+            outModelVect{iM,1}.mName = opts.modelNames{iM};
+        else
+            outModelVect{iM,1}.mName = num2str(iV);
+        end
+        continue;
     end
     
-    % plotting fitted model
-    if plotOpts.plotCI
-        % confidecte intervals preparation
-        modelfun = @(parameterVector, designMatrix) designMatrix*parameterVector';
-        [ageS,sortID]=sort(model.input.age);
-        beta=plModel.beta(1:end-nCov)';
-        desMat=plModel.designMatrix(sortID,1:end-nCov);
-        R=plModel.stats.iwres(sortID);
-        CovB=plModel.stats.covb(1:end-nCov,1:end-nCov);
-        [ypred,delta] = nlpredci(modelfun,desMat,beta,R,'Covar',CovB);
-        
-        plotDataAndFit(model.input.age,[],[],params,plotCol{end},ageS,ypred,delta);
+    for iO = 1:nOrders
+        fprintf('%d ',opts.orders(iO));
+        estOpts.mOrder=opts.orders(iO);
+        estOpts.groupEffect=1;
+        estOpts.interEffect=1;
+        estOpts.mType=opts.mType;
+        % estimate temporary model
+        if ~isempty(input.cov)
+            tmpModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                input.age(dataID,:), input.cov(dataID,:), dataVect(dataID,:), estOpts);
+        else
+            tmpModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                input.age(dataID,:), [], dataVect(dataID,:), estOpts);
+        end
+        if iO==1
+            outModelVect{iM,1}=tmpModel; % save constant model
+        elseif tmpModel.stats.bic < outModelVect{iM}.stats.bic-2
+          outModelVect{iM,1}=tmpModel; % keep higher order model if BIC decreases
+        else
+            break; % if bic does not decrease sufficiently enough, stop increasing model order (not necessary to compute higher order models)
+        end
+    end
+    
+    if isfield(opts,'modelNames')
+        outModelVect{iM,1}.mName = opts.modelNames{iM};
     else
-        plotDataAndFit(model.input.age,[],[],params,plotCol{end});
+        outModelVect{iM,1}.mName = num2str(iV);
     end
+    
+    
+    fprintf('\nFinal model order: %d ',outModelVect{iM}.order);
+    
+    if length(unique(input.grouping))>1 % if there is more than one group
+        %% likelihood ratio test for significant group effect
+        estOpts.groupEffect=0;
+        estOpts.interEffect=0;
+        estOpts.mOrder=outModelVect{iM}.order;
+        estOpts.mType=opts.mType;
+        if ~isempty(input.cov)
+            reducedModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                input.age(dataID,:), input.cov(dataID,:), dataVect(dataID,:), estOpts);
+        else
+            reducedModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                input.age(dataID,:), [], dataVect(dataID,:), estOpts);
+        end
 
-    plottedModel = 'noGroup';
+        % degrees of freedom of the chi-squared distribution for the likelihood ratio test is de difference dofs of every model
+        dof = length(outModelVect{iM}.beta)-length(reducedModel.beta);
+        [outModelVect{iM}.groupEffect.h outModelVect{iM}.groupEffect.p outModelVect{iM}.groupEffect.dof_diff outModelVect{iM}.groupEffect.Chi2] = likelihoodratiotest(outModelVect{iM}.stats.logl, reducedModel.stats.logl, dof, opts.alpha);
+        outModelVect{iM}.groupEffect.reducedModel = reducedModel;
     
-elseif model.order && ~strcmp(plotOpts.plotType,'full') && isfield(model,'interEffect') && ~model.interEffect.h % no significant age by group effect
-    % plot data
-    for iG=1:length(groups)
-        pl(iG) = plotDataAndFit(age{iG},data{iG},subjects{iG},[],plotCol{iG});
-    end
-    
-    
-    plModel = model.interEffect.reducedModel;
-    for iG = 1:length(groups)
-        R{iG} = plModel.stats.iwres(grouping==groups(iG));
-        % get model parameters
-        params{iG}(1)= plModel.beta(1) + plModel.beta(2)*groups(iG);
-        for iP = 2:length(plModel.beta)-nCov-1
-            params{iG}(iP) = plModel.beta(iP+1);
-        end
-    end
-    
-    % plotting fitted model per group
-    if plotOpts.plotCI
-        % confidecte intervals preparation
-        modelfun = @(parameterVector, designMatrix) designMatrix*parameterVector';
-        [ageS,sortID]=sort(model.input.age);
-        beta=plModel.beta(1:end-nCov)';
-        desMat=plModel.designMatrix(sortID,1:end-nCov);
-        R=plModel.stats.iwres(sortID);
-        CovB=plModel.stats.covb(1:end-nCov,1:end-nCov);
-        [ypred,delta] = nlpredci(modelfun,desMat,beta,R,'Covar',CovB);
-        
-        % plot fitted models with confidence intervals
-        for iG=1:length(groups)
-            y{iG}=ypred(grouping(sortID)==groups(iG));
-            delt{iG}=delta(grouping(sortID)==groups(iG));
-            ageSort{iG}=ageS(grouping(sortID)==groups(iG));
-            
-            plotDataAndFit(age{iG},[],[],params{iG},plotCol{iG},ageSort{iG},y{iG},delt{iG});
-        end
-    else
-        % plot fitted models on top
-        for iG=1:length(groups)         
-            plotDataAndFit(age{iG},[],[],params{iG},plotCol{iG});
-        end
-    end
-    
-    plottedModel = 'noInteraction';
-else
-    % plot data
-    for iG=1:length(groups)
-        pl(iG) = plotDataAndFit(age{iG},data{iG},subjects{iG},[],plotCol{iG});
-    end
-    
-    
-    
-    plModel = model;
-    if length(groups)>1
-        for iG = 1:length(groups)
-            R{iG} = plModel.stats.iwres(grouping==groups(iG));
-            % get model parameters
-            for iP = 1:plModel.order+1
-                params{iG}(iP) = plModel.beta(2*iP-1)+plModel.beta(2*iP)*groups(iG);
+        %% likelihood ratio test for significant age*group interaction
+        if outModelVect{iM}.order
+            estOpts.groupEffect=1;
+            estOpts.interEffect=0;
+            estOpts.mOrder=outModelVect{iM}.order;
+            estOpts.mType=opts.mType;
+            if ~isempty(input.cov)
+                reducedModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                    input.age(dataID,:), input.cov(dataID,:), dataVect(dataID,:), estOpts);
+            else
+                reducedModel = estimateModel(input.subjID(dataID,:), input.grouping(dataID,:),...
+                    input.age(dataID,:), [], dataVect(dataID,:), estOpts);
             end
-        end
-    else
-        R{1} = plModel.stats.iwres;
-        % get model parameters
-        params{1} = plModel.beta(1:end-nCov);
-    end
-    
-    % plotting fitted model
-    if plotOpts.plotCI
-        % confidecte intervals preparation
-        modelfun = @(parameterVector, designMatrix) designMatrix*parameterVector';
-        [ageS,sortID]=sort(model.input.age);
-        beta=plModel.beta(1:end-nCov)';
-        desMat=plModel.designMatrix(sortID,1:end-nCov);
-        R=plModel.stats.iwres(sortID);
-        CovB=plModel.stats.covb(1:end-nCov,1:end-nCov);
-        [ypred,delta] = nlpredci(modelfun,desMat,beta,R,'Covar',CovB);
-        
-        % plot fitted models with confidence intervals
-        for iG=1:length(groups)
-            y{iG}=ypred(grouping(sortID)==groups(iG));
-            delt{iG}=delta(grouping(sortID)==groups(iG));
-            ageSort{iG}=ageS(grouping(sortID)==groups(iG));
-            
-            plotDataAndFit(age{iG},[],[],params{iG},plotCol{iG},ageSort{iG},y{iG},delt{iG});
-        end
-    else
-        % plot fitted models on top
-        for iG=1:length(groups)         
-            plotDataAndFit(age{iG},[],[],params{iG},plotCol{iG});
+            dof = length(outModelVect{iM}.beta)-length(reducedModel.beta);
+            [outModelVect{iM}.interEffect.h outModelVect{iM}.interEffect.p outModelVect{iM}.interEffect.dof_diff outModelVect{iM}.interEffect.Chi2] = likelihoodratiotest(outModelVect{iM}.stats.logl, reducedModel.stats.logl, dof, opts.alpha);
+            outModelVect{iM}.interEffect.reducedModel = reducedModel;
         end
     end
-    
-    plottedModel = 'full';
 end
-
-%% plot labels
-% title
-if isfield(plotOpts,'titletext')
-    title(plotOpts.titletext)
-else
-    if ~isfield(model,'groupEffect')
-        title(['model order: ' num2str(model.order)]);
-    elseif model.order
-        title(['model order: ' num2str(model.order) ...
-            ',   p-val group effect: ' num2str(model.groupEffect.p,'%.4f') ...
-            ',   p-val interaction: ' num2str(model.interEffect.p,'%.4f')]);
-    else
-        title(['model order: ' num2str(model.order) ...
-            ',   p-val group effect: ' num2str(model.groupEffect.p,'%.4f')]);
-    end
 end
-
-% xlabel
-if isfield(plotOpts,'xLabel')
-    xlabel(plotOpts.xLabel);
-else
-    xlabel('age')
-end
-
-% ylabel
-if isfield(plotOpts,'yLabel')
-    ylabel(plotOpts.yLabel);
-else
-    ylabel('data');
-end
-
-% legend
-if isfield(plotOpts,'legTxt')
-    legend(pl,plotOpts.legTxt)
-else
-    for iG=1:length(groups)
-        legTxt{iG}=['Group ' num2str(iG)];
-    end
-    legend(pl,legTxt);
-end
-
-
-
-
-
