71f01564d12b26b161203ab2d6402690c5544177
karinbo <63347082+karinbo@users.noreply.github.com>
2020-05-13 09:11:24 +0200

    Update GroupCalculationEffect.m

--- functions/GroupCalculationEffect.m
+++ functions/GroupCalculationEffect.m
@@ -3,10 +3,10 @@ i=0;
 for iM=1:size(outModelVect,1)
 if isfield(outModelVect{iM,1}, 'groupEffect')
     if iM==1
-    effectSizeGroup=table(iM, outModelVect{1,1}.groupEffect.Chi2, outModelVect{1,1}.groupEffect.dof_diff, outModelVect{1,1}.groupEffect.p);
+    effectSizeGroup=table(iM, outModelVect{iM,1}.groupEffect.Chi2, outModelVect{iM,1}.groupEffect.dof_diff, outModelVect{iM,1}.groupEffect.p);
     effectSizeGroup.Properties.VariableNames={'Number of model','Chi square statistics','Degree of fredoom','p-value'};
     else 
-    effectSizeGroupAdd=table(iM, outModelVect{1,1}.groupEffect.Chi2, outModelVect{1,1}.groupEffect.dof_diff, outModelVect{1,1}.groupEffect.p);
+    effectSizeGroupAdd=table(iM, outModelVect{iM,1}.groupEffect.Chi2, outModelVect{iM,1}.groupEffect.dof_diff, outModelVect{iM,1}.groupEffect.p);
     effectSizeGroupAdd.Properties.VariableNames={'Number of model','Chi square statistics','Degree of fredoom','p-value'};
     effectSizeGroup=[effectSizeGroup;effectSizeGroupAdd];   
     end
